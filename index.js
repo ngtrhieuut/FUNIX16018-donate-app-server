654c802076e8485d87ae77fca3b020de05583837
Hieu Nguyen <102361395+ngtrhieuut@users.noreply.github.com>
2022-07-19 14:07:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ app.use(cors());
 app.use(cookieParser())
 dotenv.config();
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res)=>{
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 
 mongoose
